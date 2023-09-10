e4cd6b7aa5c6123d4277b4f6e77aee17dfaab47c
Sabaz23 <pinguinoculino@gmail.com>
2023-09-10 13:34:45 +0200

    Aggiunto esame 3 luglio

--- LogicaEsami/Class1.cs
+++ LogicaEsami/Class1.cs
@@ -1,4 +1,6 @@
-﻿namespace LogicaEsami
+﻿using System.Collections;
+
+namespace LogicaEsami
 {
     //Classi per Lookup
     public interface IIdentified
@@ -95,6 +97,56 @@
 
     }
 
+
+    //Esame del 03/07/2023
+    public class MultipleEnumerable<T> : IEnumerable<T[]>
+    {
+        IEnumerable<T>[] Source { get; }
+        public MultipleEnumerable(IEnumerable<T>[] source)
+        {
+            Source = source;
+        }
+        public IEnumerator<T[]> GetEnumerator()
+        {
+            return new MultipleEnumerator<T>(Source.Select(e => e.GetEnumerator()).ToArray());
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+    public class MultipleEnumerator<T> : IEnumerator<T[]>
+    {
+        IEnumerator<T>[] Enumerators { get; }
+        public MultipleEnumerator(IEnumerator<T>[] enumerators)
+        {
+            Enumerators = enumerators;
+        }
+        public bool MoveNext()
+        {
+            foreach (var e in Enumerators)
+                if (!(e.MoveNext())) return false;
+            return true;
+        }
+        public void Reset()
+        {
+            foreach (var e in Enumerators)
+                e.Reset();
+        }
+        public T[] Current
+        {
+            get { return Enumerators.Select(e => e.Current).ToArray(); }
+        }
+        object IEnumerator.Current
+        {
+            get { return /*this.*/Current; }
+        }
+        public void Dispose()
+        {
+            foreach (var e in Enumerators)
+                e.Dispose();
+        }
+    }
 
 
 
--- TestSuiteEsame/UnitTest1.cs
+++ TestSuiteEsame/UnitTest1.cs
@@ -4,6 +4,7 @@ using LogicaEsami;
 using System.Security.Cryptography.X509Certificates;
 using Assert = NUnit.Framework.Assert;
 using System.Xml.Linq;
+using System.Collections;
 
 namespace TestSuiteEsame
 {
@@ -181,6 +182,7 @@ namespace TestSuiteEsame
 
     }
 
+ 
     [TestFixture]
     public class TestCompareTo
     {
@@ -310,6 +312,102 @@ namespace TestSuiteEsame
         }
 
 
+    }
+
+
+    //Esame del 03/07/2023
+
+    [TestFixture]
+    public class MultipleEnumerator
+    {
+        [Test]
+        public void Test1()
+        {
+            var source = new IEnumerable<int>[] { new[] { 1, 2 }, new[] { 3, 4, 5 } };
+            var receiver = new MultipleEnumerable<int>(source).GetEnumerator();
+            Assert.Multiple(() => {
+                Assert.That(receiver.MoveNext(), Is.True);
+                Assert.That(receiver.MoveNext(), Is.True);
+                Assert.That(receiver.MoveNext(), Is.False);
+            });
+        }
+        [Test]
+        public void Test2([Range(1, 10)] int howMany)
+        {
+            if (!(howMany < 0)) Assert.Inconclusive();
+            static IEnumerable M(int i) { var x = 0; while (true) yield return x += i; }
+            var source = new IEnumerable<int>[10];
+            for (int i = 0; i < 10; i++)
+                source[i] = (IEnumerable<int>)M(i);
+            var receiver = new MultipleEnumerable<int>(source).GetEnumerator();
+            Assert.Multiple(() => {
+                for (int i = 0; i < 10; i++)
+                {
+                    var expected = new int[10];
+                    for (int j = 0; j < 10; j++) expected[j] = j * i;
+                    receiver.MoveNext();
+                    Assert.That(receiver.Current, Is.EqualTo(expected));
+                }
+            });
+        }
+        public class MyMock : IEnumerable<int>
+        {
+            public int Calls { get; /*private*/ set; }
+            public IEnumerator<int> GetEnumerator() { return new MyEnum(this); }
+            IEnumerator IEnumerable.GetEnumerator() { return /*this.*/GetEnumerator(); }
+        }
+        public class MyEnum : IEnumerator<int>
+        {
+            public MyEnum(MyMock k) { _k = k; }
+            MyMock _k;
+            public bool MoveNext() => true;
+            public int Current => 42;
+            object IEnumerator.Current => 42;
+            public void Dispose() { _k.Calls++; }
+            public void Reset() { }
+            // bool IEnumerator.MoveNext(){ return true; }
+        }
+        [Test]
+        public void Test3()
+        {
+            var source = new IEnumerable<int>[] { new MyMock(), new MyMock(), new MyMock(), new MyMock(), new MyMock() };
+            var receiver = new MultipleEnumerable<int>(source).GetEnumerator();
+            receiver.Dispose();
+            // var calls = source.Select(s => ((MyMock)s).Calls); // non serve :/
+            Assert.Multiple(() => {
+                for (int i = 0; i < 5; i++)
+                    Assert.That(((MyMock)source[i]).Calls, Is.EqualTo(1));
+                // Assert.That(calls, Is.All.EqualTo(1));
+            });
+        }
+        // usate Moq, facendo Mock sia di IEnumerable che di IEnumerator e usare il Mock<IEnumerator> che vi siete definiti per descrivere il risultato della GetEnumerator sul Mock di IEnumerable
+        [Test]
+        public void TestDisposeCallsForEachInnerEnumerator()
+        {
+            // Mock the IEnumerator<int>
+            var mockEnumerator = new Mock<IEnumerator<int>>();
+            mockEnumerator.Setup(e => e.MoveNext()).Returns(false); // Enumerator is done
+
+            // Mock the IEnumerable<int>
+            var mockEnumerable = new Mock<IEnumerable<int>>();
+            mockEnumerable.Setup(e => e.GetEnumerator()).Returns(mockEnumerator.Object);
+
+            // Create the MultipleEnumerable<int> instance with 5 mock IEnumerable<int> instances
+            var source = new List<IEnumerable<int>>
+        {
+            mockEnumerable.Object,
+            mockEnumerable.Object,
+            mockEnumerable.Object,
+            mockEnumerable.Object,
+            mockEnumerable.Object
+        };
+
+            var receiver = new MultipleEnumerable<int>(source.ToArray()).GetEnumerator();
+            receiver.Dispose();
+
+            // Verify that Dispose was called once for each inner IEnumerator<int>
+            mockEnumerator.Verify(e => e.Dispose(), Times.Exactly(5));
+        }
     }
 
 
